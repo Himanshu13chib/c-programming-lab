4b76cf3cfea925a23b80a67a6fc1a35d22c53718
Himanshu13chib <147174871+Himanshu13chib@users.noreply.github.com>
2023-10-09 20:53:42 +0530

    Add files via upload

---
+++ program to print helloworld.c
@@ -0,0 +1,20 @@
+//program to print hello world
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    return 0;
+}
+
+//program to print hello world using  new line character
+#include<stdio.h>
+int main(){
+    printf("helloworld\nhellowworld");
+    return 0;
+}
+
+//program to print hello world using tab character
+#include<stdio.h>
+int main(){
+    printf("helloworld\thelloworld");
+    return 0;
+}
