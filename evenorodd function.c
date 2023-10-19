917798a3637ba12fabaf51414d067ccd874a041f
Himanshu13chib <147174871+Himanshu13chib@users.noreply.github.com>
2023-10-19 22:21:13 +0530

    Add files via upload

---
+++ evenorodd function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the no. for a");
+    scanf("%d",&a);
+    (a%2==0)? printf("%d its even no.\n",a): printf("%d its odd no.\n",a);
+    return 0;
+}
+
+
+
+   
